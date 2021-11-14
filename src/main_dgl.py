82acb8359a6c33df3b5b85c94bbd04afd35ca02f
jaehoo98 <51286116+jaehoo98@users.noreply.github.com>
2021-11-14 21:48:25 +0900

    Update main_dgl.py

--- src/main_dgl.py
+++ src/main_dgl.py
@@ -1,10 +1,19 @@
 from data_preprocessing import get_dataframe_from_dataset
-from generate_graphs import generate_dgl_graph
+from generate_graphs import generate_stellar_graph, generate_dgl_graph
+from dgl_node2vec import dgl_node2vec
+from construct_movie_graph import construct_movie_graph
 
 
 data = get_dataframe_from_dataset(["data/toy_dataset/raw/ratings.txt"])
-print(data)
+movie_graph = construct_movie_graph(data)
+print(movie_graph)
 
-dgl_graph = generate_dgl_graph(data)
+#user_graph = construct_user_graph(data)
+
+dgl_graph = generate_dgl_graph(movie_graph)
+
 print("--- DGL GRAPH ---")
 print(dgl_graph)
+print("----- DGL node2vec -----")
+node2vec = dgl_node2vec(dgl_graph, p=1, q=1, Walk_length=6, return_edge_ids=False)
+print(node2vec)
